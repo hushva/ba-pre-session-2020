ae91bb4b693e390a169e09e43940f12a5f77573e
hushva <71833817+hushva@users.noreply.github.com>
2020-09-27 14:52:01 +0200

    adding the script and txts and math pictures

--- exam/Takacs_Karola/IT/my_script.sh
+++ exam/Takacs_Karola/IT/my_script.sh
@@ -1,5 +1,3 @@
-mkdir /made_by_my_script
-echo "Hello world"
-ls > made_by_my_script/hello.txt
-ls made_by_my_script
-ls > made_by_my_script/list_of_files.txt
+mkdir made_by_my_script
+echo "Hello world"  > made_by_my_script/hello.txt
+ls made_by_my_script  > made_by_my_script/list_of_files.txt
